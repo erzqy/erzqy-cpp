5f8100d2db45b4cb3ff1075526ac85d4589252ea
erzqy <riezqy189@gmail.com>
2019-04-24 20:51:39 +0800

    adding jones.cpp for nothing.

---
+++ jones.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#define JONES int
+#define JONEs main
+#define JONeS (
+#define JOnES )
+#define JoNES {
+#define jONES std
+#define JONes ::
+#define JOneS cout
+#define JonES <<
+#define joNES "joNES"
+#define jONEs "\n"
+#define JOnes ;
+#define JoneS return
+#define joneS 0
+#define jonEs }
+
+JONES JONEs JONeS JOnES JoNES
+  jONES JONes JOneS JonES joNES JonES jONEs JOnes
+  JoneS joneS JOnes
+jonEs
